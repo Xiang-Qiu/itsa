2325e1200ca902ca566935a96e68eba6416cbf81
Xiang-Qiu <135586719+Xiang-Qiu@users.noreply.github.com>
2024-01-12 20:54:23 +0800

    Create itsa15.c

---
+++ itsa15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("NT10=%d\n", a/10);
+    printf("NT5=%d\n", (a%10)/5);
+    printf("NT1=%d\n", a%5);
+}
